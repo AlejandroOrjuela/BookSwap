e7401d9a5e3ff85db303ff8dcfaad0ce02465192
somebody125125 <88913043+somebody125125@users.noreply.github.com>
2023-02-07 18:19:46 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,3 @@
+<?php
+$conexion = pg_connect("dbname=inventario host=localhost port=5432 user=postgres password=27852834ale");
+?>
--- index.php
+++ index.php
@@ -7,14 +7,26 @@
 <body>
   <header>
     <nav>
+    <img href="index.php" src="img/lib.png">
       <div class="search-container">
         <form action="buscar.php">
           <input type="text" placeholder="Buscar productos">
-          <button type="submit">Buscar</button>
+          <button type="submit">Ir</button>
         </form>
+        <form action="buscar.php">
+        <select id="categoria" name="categoria">
+        <option value="">Selecciona una categoría</option>
+        <option value="libros">Libros</option>
+        <option value="electronica">Electrónica</option>
+        <option value="ropa">Ropa</option>
+      </select>
+      
+      <button type="submit">Filtrar</button>
+    </form>
+  
       </div>
       <ul>
-        <li><a href="#">Inicio</a></li>
+        <li><a href="index.php">Inicio</a></li>
         <li><a href="#">Carrito de compra</a></li>
         <li><a href="#">Iniciar sesión</a></li>
         <li><a href="#">Subir producto</a></li>
@@ -25,23 +37,10 @@
     <h1>Bienvenido a nuestra tienda en línea</h1>
     <p>Aquí encontrarás una amplia variedad de productos para todos los gustos y presupuestos.</p>
     <section class="products">
-      <h2>Destacados</h2>
       <div class="product">
-        <img src="product1.jpg" alt="Product 1">
+        <img src="img/lib.png" alt="Product 1">
         <h3>Producto 1</h3>
         <p>$100</p>
-        <button>Añadir al carrito</button>
-      </div>
-      <div class="product">
-        <img src="product2.jpg" alt="Product 2">
-        <h3>Producto 2</h3>
-        <p>$200</p>
-        <button>Añadir al carrito</button>
-      </div>
-      <div class="product">
-        <img src="product3.jpg" alt="Product 3">
-        <h3>Producto 3</h3>
-        <p>$150</p>
         <button>Añadir al carrito</button>
       </div>
     </section>
