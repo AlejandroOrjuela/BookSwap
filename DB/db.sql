259bbd817ac2b3fa4d5cfcbb8dc8f6f9ed3bfa29
somebody125125 <88913043+somebody125125@users.noreply.github.com>
2023-02-07 16:50:30 -0500

    Update
    estaba molestando con mas de 1 serial como llave primaria

--- DB/db.sql
+++ DB/db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE usuario(
-    id_u serial primary key,
+    id_u varchar(20) primary key,
     nomb_u varchar(50),
     fecha_nac date,
     vendedor boolean,
@@ -7,23 +7,23 @@ CREATE TABLE usuario(
 );
 
 CREATE TABLE publicacion(
-    id_p serial primary key,
+    id_p varchar(10) primary key,
     nomb_p varchar(20),
     precio int,
     descr varchar(150),
     disp boolean,
-    id_u serial
+    id_u varchar(20)
 );
 
-ALTER TABLE pubiclacion ADD CONSTRAINT fk_id_u1 FOREIGN KEY (id_u) REFERENCES usuario(id_u);
+ALTER TABLE publicacion ADD CONSTRAINT fk_id_u1 FOREIGN KEY (id_u) REFERENCES usuario(id_u);
 
 CREATE TABLE compra(
     id_c serial primary key,
     nomb_l varchar(20),
     fecha date,
     pagado boolean,
-    id_u serial,
-    id_p serial
+    id_u varchar(10),
+    id_p varchar(10)
 );
 
 ALTER TABLE compra ADD CONSTRAINT fk_id_u2 FOREIGN KEY (id_u) REFERENCES usuario(id_u);
